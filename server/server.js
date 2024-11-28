4fc11377b3cbcd8c4b0753b65b071b0e03d2bd63
baskhuu <baskhuu@gmail.com>
2024-11-28 17:01:02 +0900

    12[schema]

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+//Creating Apollo Server object type to create Schema ([1]GraphQL's schema definition language (SDL))
 const typeDefs = `
     type Query {
         hello:  String
@@ -5,9 +9,22 @@ const typeDefs = `
 }
 `;
 
+//This is [2]Resolver Function!
 const resolvers = {
     Query:  {
         hello: () => 'Hello Basaa!',
         ognoo: () => new Date().toLocaleString()
     } ,
 };
+
+//[3]Starting Apollo Server using with [1] and [2].
+const server = new ApolloServer({
+    typeDefs,
+    // typeDefs: typeDefs, // The above text is an abbreviated text!
+    resolvers,
+});
+
+//Waiting promise from [3] and deconstruct url from result.
+const { url } = await startStandaloneServer(server);
+
+console.log(` Server ready at ${url}`);
